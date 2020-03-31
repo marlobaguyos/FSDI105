6a30f5e9d84b6ffd48343624a19ad95d5742221a
Marlo <marlo.baguyos@outlook.com>
2020-03-31 09:36:40 -0400

    update

--- TravelSite/script.js
+++ TravelSite/script.js
@@ -1,21 +1,17 @@
 var logo = document.getElementById('logo');
 var links = document.getElementsByTagName('a');
+var btns = document.getElementsByClassName('boton');
 
 var a = document.getElementById('navegacion').getElementsByTagName('a');
-a[0].setAttribute('href', 'http//www.google.com');
+a[0].setAttribute('href', 'http://www.google.com');
 console.log(a);
-
-
-// var a = document.getElementById('menu').getElementsByTagName('a');
-// a[0].setAttribute('href', 'http://www.google.com');
-
 
 for(let i=0;i<links.length;i++){
     links[i].setAttribute('target', '_blank');
 }
 
-// for(let j=0;j<btns.length;j++){
-//     btns[j].setAttribute('class', 'x');
-// }
+for(let j=0;j<btns.length;j++){
+    btns[j].setAttribute('class', 'x');
+}
 
 // var footer = document.querySelector();
