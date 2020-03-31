56c88339a506ec17638a1cab71018bf6923f0bfc
Marlo <marlo.baguyos@outlook.com>
2020-03-30 21:30:05 -0400

    update

--- TravelSite/script.js
+++ TravelSite/script.js
@@ -5,13 +5,16 @@ var a = document.getElementById('navegacion').getElementsByTagName('a');
 a[0].setAttribute('href', 'http//www.google.com');
 console.log(a);
 
+for(let i=0; i<links.length; i++){
+    links[i].setAttribute('target', '_blank');
+}
 
 // var a = document.getElementById('menu').getElementsByTagName('a');
 // a[0].setAttribute('href', 'http://www.google.com');
 
 
 // for(let i=0;i<links.length;i++){
-//     links[i].setAttribute('target', '_blank');
+//     links[i].setAttribute('target', '_blank');s
 // }
 
 // for(let j=0;j<btns.length;j++){
