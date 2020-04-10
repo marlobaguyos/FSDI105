99c17656d6d6c35f915052e00b574aa927e5766b
Marlo <marlo.baguyos@outlook.com>
2020-04-10 10:40:55 -0400

    update

--- TravelSite/script2.js
+++ TravelSite/script2.js
@@ -7,6 +7,13 @@ $(function(){
 
     // $('main article:last p:first').remove();
 
-    $('main article:first h2').text("Hello");
+    // $('main article:first h2').text("Hello");
+    // $('main article:last h2').html("<h1>Pogi</h1>");
+
+    jQuery('.navegacion ul li:first a').attr('href', "http://udemy.com");
+
+    $('main article:first img').on('click', function(){
+        $(this).attr('src', 'img/imagen_2.jpg');
+    });
 
 })
