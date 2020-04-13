6bb745d02d5db33e037b39155ea3a00bf39a652a
Marlo <marlo.baguyos@outlook.com>
2020-04-13 11:53:29 -0400

    update

--- TravelSite/script2.js
+++ TravelSite/script2.js
@@ -1,5 +1,5 @@
-$(function(){
-    'use strict';
+//$(function(){
+    //'use strict';
     
     // $('.enlace').hide();
 
@@ -10,13 +10,13 @@ $(function(){
     // $('main article:first h2').text("Hello");
     // $('main article:last h2').html("<h1>Pogi</h1>");
 
-    jQuery('.navegacion ul li:first a').attr('href', "http://udemy.com");
+    // jQuery('.navegacion ul li:first a').attr('href', "http://udemy.com");
 
-    $('main article:first img').on('click', function(){
-        $(this).attr('src', 'img/imagen_2.jpg');
-    });
+    // $('main article:first img').on('click', function(){
+    //     $(this).attr('src', 'img/imagen_2.jpg');
+    // });
 
-    var copy = $('main article:last').clone();
+    // var copy = $('main article:last').clone();
 
     // $('main').append(copy);
     // $(copy).appendTo('main');
@@ -24,14 +24,45 @@ $(function(){
     // $('main').prepend(copy);
     // $(copy).prependTo('main');
 
-    $('div.logo img').on('mouseleave', function(){
-        //body of the function
-        console.log('mouse leave');
+    // $('div.logo img').on('mouseleave', function(){
+    //     //body of the function
+    //     console.log('mouse leave');
+    // });
+
+    // $('div.logo img').on('mouseenter',function(){
+    //     // $(this).remove();
+    //     console.log('mouse enter ')
+    // })
+
+//})
+
+// $(function(){
+//     'use strict';
+//     $('.logo img').on('click',function(){
+//         $(this).animate({'width':'200px'}, 1000);
+//     });
+
+//     $('main article img').on('mouseenter', increasing);
+//     $('main article img').on('mouseleave', reducing);
+
+//     function increasing(){
+//         $(this).animate({'width': '100%'});
+//     }
+
+//     function reducing(){
+//         $(this).animate({'width': '350px'});
+//     }
+
+// });
+
+$(function(){
+    'use strict';
+    $('.logo img').click(function(){
+        $('main article:first').slideUp(1000);
     });
 
-    $('div.logo img').on('mouseenter',function(){
-        // $(this).remove();
-        console.log('mouse enter ')
-    })
+    $('aside').click(function(){
+        $('main article:first').slideDown(1000);
+    });
 
-})
+});
