a823a6dfc1545b71faa3775a7c743c1edfe53e32
marlo <marlo.baguyos@outlook.com>
2020-04-20 10:23:52 -0400

    update

--- petSalon/myscript.js
+++ petSalon/myscript.js
@@ -19,7 +19,7 @@ const salon = {
 
 let {name,phone,address:{street,number},workingHours:{days,open,close}} = salon;
 
-document.querySelector('.info').innerHTML=`Contact us ${salon.phone}, ${salon.street} ${salon.number} <br> It's open from ${salon.address.open} to ${salon.address.close}`;
+// document.querySelector('.info').innerHTML=`Contact us ${salon.phone}, ${salon.street} ${salon.number} <br> It's open from ${salon.address.open} to ${salon.address.close}`;
 
 
 //object constructor
